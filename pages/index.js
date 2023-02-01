1dec256cdfc52bc69b38a6b86e95f5bb8bf2eed9
Akin GURLER <akingurler98@hotmail.com>
2023-02-01 18:10:12 +0300

    recent posts request and display

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,28 @@
+import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
 
-import Head from 'next/head'
-import Image from 'next/image'
-import {PostCard,Categories,PostWidget, Header} from "../components/index.js"
-import {getPosts} from '../services'
-
-
-
-export default function Home( {posts} ) {
+export default function Home({ posts }) {
   return (
-    /* ----------------- Container Start------------ */
-    <div className="container mx-auto px-10 mb-10 ">
-      <Head>
-        <title>Blog Akın</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header/>
-
-      {/* ----------- Post-And-Categories-------------- */}
-      {/* Normalde Postlar ve kategori kısmı aynı boyutta iki kolonken large devicelarda 12 lik bir bölünme yapılıp 8 e 4 olacak */}
-    <div className='grid grid-cols-2 lg:grid-cols-12 gap-8 '>
-      {/* ---------Posts----------- */}
-      <div className="lg:col-span-8 col-span-1 bg-blue-200">
-         {posts.map((post)=>(<PostCard post={post.node} key={post.title}/>))}
-      </div>
-      {/* ---------Posts--End--------- */}
-     {/* ------------Categories---------------- */}
-      <div className="lg:col-span-4 col-span-1">
-          <div className="lg:sticky relative top-8 bg-red-200">
-          <PostWidget />
-          <Categories />
+    <div className="container mx-auto px-10 mb-8">
+     
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
           </div>
+        </div>
       </div>
-      {/* ------------Categories-END------------- */}
     </div>
-       {/* ----------- Post-And-Categories------------- */}
-    </div>
-    /* ----------------- Container End ------------ */
-  )
+  );
 }
 
+// Fetch data at build time
 export async function getStaticProps() {
   const posts = (await getPosts()) || [];
   return {
--- services/index.js
+++ services/index.js
@@ -1,6 +1,6 @@
 
 import { request, gql } from 'graphql-request';
-const graphqlAPI = process.env.GRAPHCMS_ENDPOINT;
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
 export const getPosts = async () => {
     const query = gql`
@@ -37,3 +37,46 @@ export const getPosts = async () => {
 
     return result.postsConnection.edges;
 }
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
+
+
+export const getSimilarPosts = async (categories, slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query, { slug, categories });
+
+  return result.posts;
+};

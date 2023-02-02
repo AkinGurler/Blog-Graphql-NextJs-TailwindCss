d37cc3b765813596c0a3af9df8aa89de40796e87
Akin GURLER <akingurler98@hotmail.com>
2023-02-02 15:10:27 +0300

    Category Component

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Link from 'next/link'
+import { getCategories } from '../services';
 
 const Categories = () => {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories))
+  }, [])
+
   return (
-    <div>Categories</div>
+    <div className='bg-white shadow-lg rounded-lg p-8'>
+      <div className='text-xl font-semibold pb-4 border-b-2'>
+        Categories
+      </div>
+      <div className='pt-8' >
+        {categories.map(category=>(
+          <div key={category.name} >
+            <Link href={`/category/${category.slug}`} >
+             <span className='block mb-3 pb-3'>
+              {category.name}
+             </span>
+             
+            </Link>
+              
+          </div>
+        ))}
+      </div>
+
+
+    </div>
   )
 }
 
